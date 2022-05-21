22ddf34b526203b00477da3d70faa541db30e929
helene9127 <105607590+helene9127@users.noreply.github.com>
2022-05-21 16:32:11 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+//let response=await fecth(
+//"AIzaSyBKmYMqOjKKVp08cVdxEK0xcGuupJJxGjk"
+//)
+
+//let cache=document.querySelector("#cache2");
+//const changeC=()=>{
+//if ('click'){
+//background-color:#ffffff
+//}
+//}
+//cache.addEventlistener ('click', changecolor)
+//
+
+// let mesForms = document.getElementById("cache3");
+// mesForms.addEventlistener("click", function () {
+// document.getElementById("monProfil").style.display = "none";
+// });
+
+document.getElementById("cache3").addEventListener("click", function () {
+  document.getElementById("mesForms").style.display="none"
+});
+
